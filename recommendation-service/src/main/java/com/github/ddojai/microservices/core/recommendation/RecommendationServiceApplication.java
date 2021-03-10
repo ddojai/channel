48d97789b6167deb78937f156c98164497ca4dad
ddojai <favianred@gmail.com>
2021-03-10 19:08:42 +0900

    Add services
    - product-service
    - recommendation-service
    - review-service
    - product-composite-service

--- recommendation-service/src/main/java/com/github/ddojai/microservices/core/recommendation/RecommendationServiceApplication.java
+++ recommendation-service/src/main/java/com/github/ddojai/microservices/core/recommendation/RecommendationServiceApplication.java
@@ -2,8 +2,10 @@ package com.github.ddojai.microservices.core.recommendation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.github.ddojai.microservices")
 public class RecommendationServiceApplication {
 
 	public static void main(String[] args) {
--- review-service/src/main/java/com/github/ddojai/microservices/core/review/ReviewServiceApplication.java
+++ review-service/src/main/java/com/github/ddojai/microservices/core/review/ReviewServiceApplication.java
@@ -2,8 +2,10 @@ package com.github.ddojai.microservices.core.review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.github.ddojai.microservices")
 public class ReviewServiceApplication {
 
 	public static void main(String[] args) {
