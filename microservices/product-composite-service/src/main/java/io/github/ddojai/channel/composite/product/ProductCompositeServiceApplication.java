05a97b0b41a268b63595385aa10e3b59310e09bc
ddojai <favianred@gmail.com>
2021-06-25 23:11:28 +0900

    Implement ProductCompositeService

--- microservices/product-composite-service/src/main/java/io/github/ddojai/channel/composite/product/ProductCompositeServiceApplication.java
+++ microservices/product-composite-service/src/main/java/io/github/ddojai/channel/composite/product/ProductCompositeServiceApplication.java
@@ -2,6 +2,8 @@ package io.github.ddojai.channel.composite.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ProductCompositeServiceApplication {
@@ -10,4 +12,8 @@ public class ProductCompositeServiceApplication {
 		SpringApplication.run(ProductCompositeServiceApplication.class, args);
 	}
 
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
