3954ac1259402939842beac1505b76970478c196
ddojai <favianred@gmail.com>
2021-08-17 22:59:11 +0900

    Add config-server to gateway routes
    - Add config-server to docker-compose

--- spring-cloud/config-server/src/test/java/io/github/ddojai/springcloud/configserver/ConfigServerApplicationTests.java
+++ spring-cloud/config-server/src/test/java/io/github/ddojai/springcloud/configserver/ConfigServerApplicationTests.java
@@ -1,13 +1,19 @@
 package io.github.ddojai.springcloud.configserver;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class ConfigServerApplicationTests {
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment=RANDOM_PORT, properties = {"spring.profiles.active=native"})
+
+public class ConfigServerApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
